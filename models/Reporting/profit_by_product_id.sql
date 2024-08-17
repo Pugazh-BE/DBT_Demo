a6627dc73060c1731dfe06f95bc614a1bdd7364a
Pugazhenthi V <erpugazh.be@gmail.com>
2024-08-17 08:03:42 +0000

    nbmvfdd

--- models/Reporting/profit_by_product_id.sql
+++ models/Reporting/profit_by_product_id.sql
@@ -1,18 +1,5 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
+{{ config(materialized="table") }}
 
-SELECT
-    PRODUCT_ID,
-    PRODUCT_NAME,
-    CATEGORY,
-    SUB_CATEGORY,
-    SUM(PROFIT) AS PROFIT
-FROM {{ ref('stg_profit') }}
-GROUP BY
-    PRODUCT_ID,
-    PRODUCT_NAME,
-    CATEGORY,
-    SUB_CATEGORY
+select product_id, product_name, category, sub_category, sum(profit) as profit
+from {{ ref("stg_profit") }}
+group by product_id, product_name, category, sub_category
