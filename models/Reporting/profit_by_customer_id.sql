8c4a3508003629942e51fbd3ae345029930da0c6
Pugazhenthi V <erpugazh.be@gmail.com>
2024-08-17 07:30:02 +0000

    fetch the profit by customer and product wise

---
+++ models/Reporting/profit_by_customer_id.sql
@@ -0,0 +1,12 @@
+SELECT
+    CUSTOMER_ID,
+    CUSTOMER_NAME,
+    SEGMENT,
+    COUNTRY,
+    SUM(PROFIT) AS PROFIT
+FROM {{ ref('stg_profit') }}
+GROUP BY 
+    CUSTOMER_ID,
+    CUSTOMER_NAME,
+    SEGMENT,
+    COUNTRY
